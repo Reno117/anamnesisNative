101a68988d8d6adaef163166938bd43b2f9e438c
Parker <parkerrenoestes@gmail.com>
2026-04-30 15:26:04 -0400

    Added help page and app icon

---
+++ app/help.tsx
@@ -0,0 +1,9 @@
+import { View, Text, Button, Linking } from "react-native";
+
+export default function HelpScreen() {
+  return (
+    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+      <Text>Need help? Contact renoestes@gmail.com</Text>
+    </View>
+  );
+}
